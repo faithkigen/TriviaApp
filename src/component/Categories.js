befc78191cd6a26b2948a22fd8276df3641c69c2
cindymuthoni <cindymuthoni14@gmail.com>
2023-07-01 14:08:50 +0300

    done

---
+++ src/component/Categories.js
@@ -0,0 +1,42 @@
+const questions = [
+    {
+      text: "USA",
+      imageUrl:"https://picsum.photos/200"
+    },
+    {
+      text: "Kenya",
+      imageUrl:"https://picsum.photos/200"
+    },
+    {
+      text: "Russia",
+      imageUrl:"https://picsum.photos/200"
+    },
+    {
+      text: "South Korea",
+      },
+      {
+      imageUrl:"https://s30876.pcdn.co/wp-content/uploads/south-korea.jpg.optimal.jpg",
+      description:""
+    },
+    {
+      text: "Pakistan",
+      imageUrl:"https://picsum.photos/200"
+    },
+    {
+      text: "Russia",
+      imageUrl:"https://picsum.photos/200",
+      description:""
+    },
+    {
+      text: "South Korea",
+      imageUrl:"https://picsum.photos/200",
+      description:""
+    },
+    {
+      text: "Pakistan",
+      imageUrl:"https://picsum.photos/200",
+      description:""
+    },
+  ];
+
+  export default questions;
--- src/component/HomePage.js
+++ src/component/HomePage.js
@@ -1,28 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import questions from './Question'
+import questions from './Question';
+import categories from './Categories';
 
 const Homepage = () => {
-  return (
-    <div className="App">
-      <h1>Trivia</h1>
-      <p>Mind game.</p>
-     
+    return (
+        <div className="App" >
+            <h1>Trivia</h1>
+            <p>Mind game.</p>
 
-      <div className="question-list">
-        {questions.map((question, index) => (
-          <div key={index} className="question">
-            <p>{question.text}</p>
-            <ul>
-              {question.options.map((option) => (
-                <li key={option.id}>{option.text}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+            <div className="p-16">
+
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+
+                    {categories.map((question, index) => (
+                        <div class="bg-gray-200 h-48 rounded-lg" style={{ backgroundImage: `url(${question.imageUrl})` }}>
+                            <div key={index} className="question">
+                                <p>{question.text}</p>
+                            </div>
+                        </div>
+
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Homepage;
